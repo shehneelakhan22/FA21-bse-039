2f361506b25f256730b9a43f7523f736be366a3f
Shehneela Khan <shehneelaaa@gmail.com>
2024-03-01 10:49:49 +0500

    Array and string Method in javascript

--- ClassAssignments/Assignment#01/arrayMethods.js
+++ ClassAssignments/Assignment#01/arrayMethods.js
@@ -1,1 +1,59 @@
+let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let strArray = ["Attock", "Lahore", "Islamabad", "Karachi", "Kashmir"];
 
+// Push Method
+strArray.push("Hyderabad");
+console.log(strArray);
+
+// Pop Method
+numArray.pop()
+console.log(numArray);
+
+// Shift Method
+strArray.shift()
+console.log(strArray);
+
+// Unshift Method
+numArray.unshift(33)
+console.log(numArray);
+
+// Length 
+console.log("Length : ",numArray.length,"elements");
+
+// at() Method
+console.log(strArray.at(1));
+
+// Join Method
+console.log(strArray.join(" *-* "));
+
+// Concat Method
+let arr1 = [1,2,3,4,5];
+let arr2 = [6,7,8,9,0];
+console.log(arr1.concat(arr2));
+
+// Delete Method
+delete numArray[2];
+console.log(numArray);
+
+// Sort Method
+let numbers = [22,33,4,55,,2222,44,11,77,999,44,0,8];
+numbers.sort();
+console.log(numbers);
+
+// Reverse Method
+numArray.reverse();
+console.log(numArray);
+
+// Slice Method
+strArray.slice(0, 3);
+console.log(strArray);
+
+// Filter Method
+let nums = [11,22,33,44,55,66,77,88,99];
+function my_filter(x){
+    if(x%2 == 0){
+        return true;
+    }
+    else return false
+}
+console.log(nums.filter(my_filter));
--- ClassAssignments/Assignment#01/stringMethods.js
+++ ClassAssignments/Assignment#01/stringMethods.js
@@ -1,1 +1,53 @@
+let myString = "Hello there!! My name is Shehneela. I am studying Software Engineering at COMSATS Attock. As university offered us the course Mobile Application Development (MAD), so we are learning the basics of JavaScript in order to build an application using React Native.";
+let FirstName = "Shehneela";
+let LastName = "Khan";
+let mytext = "      Hello there!!      ";
+let text = "Hello Guys!! What's going on?";
 
+// Length 
+console.log(myString.length);
+
+// Extract string Method 
+console.log(myString.charAt(1));
+console.log(FirstName.at(2));
+
+// Lowercase Method
+console.log(FirstName.toLowerCase());
+
+// Lowercase Method
+console.log(FirstName.toUpperCase());
+
+// Slice Method
+console.log(myString.slice(7,17));
+
+// Trim Method
+console.log(mytext);
+console.log(mytext.trim());
+
+// Concat Method
+let concatText = FirstName.concat(" ", LastName);
+console.log(concatText);
+
+// Trim Start Method
+console.log(mytext.trimStart());
+
+// Trim End Method
+console.log(mytext.trimEnd());
+
+// Repeat Method
+console.log(FirstName.repeat(3));
+
+// Replace Method
+console.log(mytext.replace("there", "Shehneela"));
+
+// Split Method
+console.log(text.split(","));
+console.log(text.split(" "));
+console.log(text.split("|"));
+
+// padStart Method
+value = "5";
+console.log(value.padStart(2, "x"));
+
+// padEnd Method
+console.log(value.padEnd(4, "0"));
